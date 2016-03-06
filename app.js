650a942cdeb3305957ac0e060bdce5aadb57adf5
freedice <noppadol.r@hotmail.com>
2016-03-06 08:56:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 988,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Play Dice Game Free with Faucet',
+  app_name: 'Play Dice Game with Free Bitcoin (Faucet)',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfsCxoTAAAAAHoSgEfleAgVqjMqHFoTgFvgstRw',  // <----- EDIT ME!
