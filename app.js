e2e00dfe0dd08989d7b08dd7096fff82802657cc
freedice <noppadol.r@hotmail.com>
2016-03-24 17:21:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -488,11 +488,11 @@ var betStore = new Store('bet', {
   Dispatcher.registerCallback('UPDATE_WAGER', function(newWager) {
     self.state.wager = _.merge({}, self.state.wager, newWager);
 
-  //  var n = parseInt(self.state.wager.str, 10);
+    var n = parseInt(self.state.wager.str, 10);
 
     // If n is a number, ensure it's at least 1 bit
     if (isFinite(n)) {
-      n = Math.max(n, 0.00010000);
+      n = Math.max(n, 0.00001);
       self.state.wager.str = n.toString();
     }
 
